b337dced6fce3eb4db0ce5760fc8989960e9cca4
sweeden-ttu <sweeden@ttu.edu>
2026-02-27 08:06:52 -0600

    cleanup

--- DataStructuresIEEE752/Sources/DataStructuresIEEE752/DataStructuresIEEE752.swift
+++
@@ -1,7 +0,0 @@
-/**
- * DataStructuresIEEE752 – Swift library for data structures conforming to
- * IEEE 752 64-bit geometrica Z field (zed) specifications.
- * Open this package in Xcode (File > Open > select this package directory) to build and test.
- */
-
-// Types are defined in IEEE752Types, TrieNode, Trie, BinaryTrieNode, TrieNode, Trie, GraphEdge.
